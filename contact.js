54fb14cbd1ab1942bb6031552f8adb6c4e165da5
Ashraf <ashraf.dham08@gmail.com>
2024-03-31 20:34:36 +0300

    Enhance the responsiveness of the website

--- contact.js
+++ contact.js
@@ -1,11 +1,34 @@
 
-function sendMail(){
-    var params={
-        from_name : document.getElementById("contactName").value,
-        email_id : document.getElementById("contactEmail").value,
-        message : document.getElementById("ContactText").value,
-    }
-    emailjs.send("service_e4gnfc7","template_isc598q",params).then(function (){
-        alert("email sent successufly")
-    })
+// function sendMail(){
+//     var params={
+//         from_name : document.getElementById("contactName").value,
+//         email_id : document.getElementById("contactEmail").value,
+//         message : document.getElementById("ContactText").value,
+//     }
+//     console.log(params);
+//     emailjs.send("service_e4gnfc7","template_isc598q",params).then(function (){
+//         alert("email sent successufly")
+//     })
+//     return false
+// }
+
+
+document.getElementById("emailForm").addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission behavior(reloading the page)
+
+    
+    var params = {
+        from_name: document.getElementById("contactName").value,
+        email_id: document.getElementById("contactEmail").value,
+        message: document.getElementById("ContactText").value,
+    };
+    emailjs.send("service_e4gnfc7", "template_isc598q", params).then(function () {
+        alert("Email sent successfully");
+    });
+    document.getElementById("contactName").value = "";
+    document.getElementById("contactEmail").value = "";
+    document.getElementById("ContactText").value = "";
+
 }
